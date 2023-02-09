f48f358232b6ca672807154c28088c8f4d3d3f70
JJ Gray <jjgray@scottlogic.com>
2023-02-09 09:59:28 +0000

    add spring starter code

---
+++ api/src/main/java/com/scottlogic/library/ScottLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.scottlogic.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScottLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScottLibraryApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/com/scottlogic/library/ScottLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scottlogic.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScottLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
